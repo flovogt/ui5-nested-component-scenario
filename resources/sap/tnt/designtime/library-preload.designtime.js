219295812455505be8575c1f9585577de560a128
flovogt <flovogt@users.noreply.github.com>
2021-03-11 16:26:52 +0000

    deploy: ab0c3ea8b1dd8ff42ba43ff9952ee7ab04ad2734

---
+++ resources/sap/tnt/designtime/library-preload.designtime.js
@@ -0,0 +1,3 @@
+//@ui5-bundle sap/tnt/designtime/library-preload.designtime.js
+sap.ui.predefine("sap/tnt/designtime/NavigationListItem.designtime",[],function(){"use strict";return{palette:{group:"ACTION"},actions:{rename:{changeType:"rename",domRef:function(e){return e.$().find(".sapTntNavLIText")[0]}}},templates:{create:"sap/tnt/designtime/NavigationListItem.create.fragment.xml"}}});
+sap.ui.predefine("sap/tnt/designtime/library.designtime",[],function(){"use strict";return{}});

a7bd6a9655e5d74230eee41a648cffb16b17505e
flovogt <flovogt@users.noreply.github.com>
2022-02-08 14:53:30 +0000

    deploy: 90a6b94825e8d163a1b222a3a68d9a2837bc25d7

--- test/integration/AllJourneys.js
+++ test/integration/AllJourneys.js
@@ -1,1 +1,1 @@
-sap.ui.define(["sap/ui/test/Opa5","./arrangements/Startup","./NavigationJourney"],function(e,t){"use strict";e.extendConfig({arrangements:new t,viewNamespace:"my.lib.sample.root.view.",timeout:2,pollingInterval:10,autoWait:true})});
+sap.ui.define(["sap/ui/test/Opa5","./arrangements/Startup","./NavigationJourney"],function(e,a){"use strict";e.extendConfig({arrangements:new a,viewNamespace:"my.lib.sample.root.view.",pollingInterval:10,autoWait:true})});

559b21c0ef712d59911583301b09677e010e006a
flovogt <flovogt@users.noreply.github.com>
2022-02-09 07:44:29 +0000

    deploy: 41c144ab4ef3b19c6ce23e9c6e43524ca7ada8e6

--- resources/sap/ui/Global-dbg.js
+++ resources/sap/ui/Global-dbg.js
@@ -77,7 +77,7 @@ sap.ui.define([
 		 */
 		version: "1.98.0",
 		// buildinfo.lastchange is deprecated and is therefore defaulted to empty string
-		buildinfo : { lastchange : "", buildtime : "20220209-0720" }
+		buildinfo : { lastchange : "", buildtime : "20220209-0743" }
 	});
 
 	var oCfgData = window["sap-ui-config"] || {};
